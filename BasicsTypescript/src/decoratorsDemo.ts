b1a8d0d16e60c2c1370f49a533e730c8b23099b4
johnsonaussie <kuang_tong@hotmail.com>
2023-04-09 18:09:10 +1000

    minor update

--- BasicsTypescript/src/decoratorsDemo.ts
+++ BasicsTypescript/src/decoratorsDemo.ts
@@ -5,7 +5,8 @@ function Helper(genericString: string) {
     }
 }
 
-function AngularTemplate(template: string, hookId: string) {
+function AngularTemplate(template: string, hookId: string) 
+{
     return function (_: any) {
         const hookEl = document.getElementById(hookId);
         if (hookEl) {
@@ -15,10 +16,14 @@ function AngularTemplate(template: string, hookId: string) {
 }
 
 // @Helper('Showing constructor:')
+// change "emitDecoratorMetadata" to true in tsconfig.json file
+// hooId is app a div with id of 'app'
 @AngularTemplate('<h4>This is a Angular like Template code</h4>', 'app')
-class Car{
-    name = 'Tata Nexon';
-    constructor(){
+class Car
+{
+    name = 'Tata Nexon of Johnson TONG';
+    constructor()
+    {
         console.log(`Car ${this.name} created`);
     }
 }
@@ -26,51 +31,61 @@ class Car{
 const car1 = new Car();
 console.log(car1)
 
+
 //Property Decorators
-function Log(target: any, propertyName: string | Symbol) {
-    console.log('Property decorator!');
+function Log(target: any, propertyName: string | Symbol) 
+{
+    console.log('From Log:Property decorator!');
     console.log(target, propertyName);
 }
 
 function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-    console.log('Accessor decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+    console.log('From Log2:Accessor decorator!');
+    console.log("target:",target);
+    console.log("name:",name);
+    console.log("PropertyDescriptor:",descriptor);
 }
 
-function Log3( target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-    console.log('Method decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+function Log3( target: any, name: string | Symbol, descriptor: PropertyDescriptor)
+ {
+    console.log('From Log3:Method decorator!');
+    console.log("target:",target);
+    console.log("name:",name);
+    console.log("PropertyDescriptor:",descriptor);
 }
 
 function Log4(target: any, name: string | Symbol, position: number) {
-    console.log('Parameter decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(position);
+    console.log('From Log4: Parameter decorator!');
+    console.log("target:",target);
+    console.log("name:",name);
+    console.log("position:",position);
 }
 
-class Employee {
+class Employee
+{
     @Log
     title: string;
     private _fullName: string;
 
     @Log2
-    set fullName(name: string) {
+    set fullName(name: string)
+     {
         this._fullName = name;
     }
 
-    constructor(title: string, name: string) {
+    constructor(title: string, name: string) 
+    {
         this.title = title;
         this._fullName = name;
     }
 
     @Log3
-    getNameWithTitle(@Log4 id: number) {
+    getNameWithTitle(@Log4 id: number)
+    {
         return `Employee - ${id}, Title - ${this.title}, Name - ${this._fullName}`;
     }
 
 }
+
+const emp2 = new Employee("CEO", "Johnson TOnG");
+console.log("getNameWithTitle:", emp2.getNameWithTitle(2));
